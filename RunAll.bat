781da4630cb0876d72859dae88b0d9c9aab224d8
TomerZv <tomerzv@gmail.com>
2016-01-05 21:24:49 +0200

    add mongo import and change server

--- RunAll.bat
+++ RunAll.bat
@@ -4,6 +4,8 @@ taskkill /F /IM chrome.exe
 start "MongoDB Server" cmd /K C:\"Program Files"\MongoDB\Server\3.2\bin\mongod.exe
 timeout 2
 start "NodeJS ADS Server" /I cmd /K "C:\Program Files\nodejs\node.exe" %CD%\server.js
+timeout 5
+start "import rows" /I cmd /K start "import rows" /I cmd /K "C:\Program Files\MongoDB\Server\3.2\bin\mongoimport.exe" --db test --collection ads --drop --jsonArray --file %CD%\data.json
 timeout 2
-start chrome.exe --allow-file-access-from-files http://127.0.0.1:8080/#/display/2
+start chrome.exe --allow-file-access-from-files http://127.0.0.1:80/#/display/1
 pause
--- server.js
+++ server.js
@@ -1,20 +1,28 @@
+var express = require("express")
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
 
-var express = require("express");
-var app = express(); // express.createServer();
 var bodyParser = require('body-parser');
-var server = require('http').Server(app);
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert')
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017/test';
 var io = require('socket.io')(server);
+var ObjectId = require('mongodb').ObjectID;
+var assert = require('assert');
 
-app.use(bodyParser.json());         // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+var mongoUrl = 'mongodb://localhost:27017/test';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
   extended: false
 })); 
 
-server.listen(8080);
+var serverPort = 80;
+var serverIp = '127.0.0.1';
+
+server.listen({
+    host: serverIp,
+    port: serverPort
+});
 
 var screens = {};
 
@@ -29,12 +37,12 @@ io.sockets.on('connection', function (socket) {
   });
 
   socket.on('ad:shown', function(data) { 
-     MongoClient.connect(url, function(err, db) {
+     MongoClient.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
         var stat = data;
         console.log(stat);
         stat.shownAt = new Date();
-        db.collection('stats').insert(stat, function(err){
+        db.collection('stats').insertOne(stat, function(err){
           console.log(err);
           console.log("Statistics Record added as " + stat._id);
           db.close();
@@ -47,152 +55,13 @@ io.sockets.on('connection', function (socket) {
     // remove the screenId from global screens list
     delete screens[socket.screenId];
   });
-    
-    
-});
 
-var insertDocuments = function(db, callback) {
-   db.collection('ads').insertMany([{
-      "title" : "הודעה 1",
-      "textLines" : ['שורה ראשונה שורה ראשונה', 'שורה שניה שורה שניה'
-                   , 'שורה שלישית שורה שלישית', 'שורה רביעית שורה רביעית'],
-      "images" : ['./images/cats.jpg', './images/animal.jpg'],
-      "templateUrl" : './templateA.html',
-      "duration" :1*60*60*12,
-      "timeFrames" : [
-          { 
-              "startDate" : "7/12/15",
-              "endDate" : "12/31/15",
-              "days" : [2,3,4,5,6],
-              "from" : "6:00 AM",
-              "to" : "11:59 PM"
-          },
-          { 
-              "startDate" : "1/1/15",
-              "endDate" : "12/31/15",
-              "days" : [4],
-              "from" : "1:00 PM",
-              "to" : "8:00 PM"
-          }
-      ],
-      "screenIds" : [1,2]
-   },{
-     "title" : "הודעה 2",
-      "textLines" : ['שורה ראשונה שורה ראשונה', 'שורה שניה שורה שניה',
-                   'שורה שלישית שורה שלישית', 'שורה רביעית שורה רביעית',
-                  'שורה חמישית שורה חמישית', 'שורה שישית שורה שישית',
-                  'שורה שביעית שורה שביעית', 'שורה שמינית שורה שמינית',
-                  'שורה תשיעית שורה תשיעית', 'שורה עשירית שורה עשירית'],
-      "images" : ['./images/sea.jpg'],
-     "templateUrl" : './templateB.html',
-      "duration" : 30,
-      "timeFrames" : [
-          { 
-               "startDate" : "3/1/15",
-              "endDate" : "7/30/15",
-              "days" : [3,4,6],
-              "from" : "8:00 AM",
-              "to" : "4:00 PM"
-          }
-      ],
-      "screenIds" : [1,3]
-  }, {
-     "title" : "הודעה 3",
-     "textLines" : [],
-      "images" : [],
-      "videos" : ['http://media.w3.org/2010/05/sintel/trailer.mp4'],
-      "templateUrl"  : './templateC.html',
-      "duration" : 20,
-     "timeFrames" : [
-          { 
-               "startDate" : "7/18/15",
-              "endDate" : "7/25/15",
-              "days" : [1,2,3,4,5,6,7],
-              "from" : "8:00 AM",
-              "to" : "10:00 PM"
-          }
-      ],
-      "screenIds" : [2,3]
-  }, {
-      "title" : "הודעה 4",
-      "textLines" : ['שורה ראשונה שורה ראשונה', 'שורה שניה שורה שניה'],
-      "images" : [],
-       "templateUrl" : './templateA.html',
-      "duration" : 20,
-      "timeFrames" : [
-          { 
-               "startDate" : "3/29/15",
-              "endDate" : "9/15/15",
-              "days" : [2,6],
-              "from" : "7:00 AM",
-              "to" : "7:00 PM"
-          }
-      ],
-       "screenIds": [1]
-  }, {
-     "title" : "הודעה 5",
-      "textLines" : ['שורה ראשונה שורה ראשונה', 'שורה שניה שורה שניה',
-                   'שורה שלישית שורה שלישית', 'שורה רביעית שורה רביעית',
-                  'שורה חמישית שורה חמישית', 'שורה שישית שורה שישית',
-                  'שורה שביעית שורה שביעית'],
-       "images": ['./images/catedral.jpg', './images/animal.jpg'],
-      "templateUrl" : './templateB.html',
-      "duration" : 30,
-      "timeFrames" : [
-          { 
-               "startDate" : "7/1/15",
-              "endDate" : "9/30/15",
-              "days" : [2,3,4,6],
-              "from" : "1:00 AM",
-              "to" : "11:00 PM"
-          }
-      ],
-      "screenIds": [3]
-  }], function(err, result) {
-    assert.equal(err, null);
-    console.log("Inserted  documents into the ads collection.");
-    callback(result);
-  });
-};
-
-var removeDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('ads');
-  // Insert some documents
-  collection.removeMany({}, function(err, result) {
-    assert.equal(err, null);
-    console.log("Delete all ads");
-    callback(result);
-  });    
-};
-
-var removeStats = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('stats');
-  // Insert some documents
-  collection.removeMany({}, function(err, result) {
-    assert.equal(err, null);
-    console.log("Delete all statistics");
-    callback(result);
-  });    
-};
-
-
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-    removeDocuments(db,function() {
-      insertDocuments(db, function() {
-         // removeStats(db, function() {
-            db.close();
-         // });
-      });
-    });
 });
 
 app.use(express.static(__dirname));
 
 app.get("/timePerAd", function(request, response){
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
         
         db.collection('stats').aggregate(
@@ -217,7 +86,7 @@ app.get("/timePerAd", function(request, response){
 });
 
 app.get("/timeUtilizationPerDay", function(request, response){
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
         
         db.collection('stats').aggregate(
@@ -242,7 +111,7 @@ app.get("/timeUtilizationPerDay", function(request, response){
 });
 
 app.get("/ads/", function(request, response){
-     MongoClient.connect(url, function(err, db) {
+     MongoClient.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
         
         var query = {};
@@ -277,11 +146,11 @@ app.get("/ads/", function(request, response){
 });
 
 app.post("/ads/", function(request, response){
-     MongoClient.connect(url, function(err, db) {
+     MongoClient.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
         var ad = request.body;
         console.log(ad);
-        db.collection('ads').insert(ad, function(err){
+        db.collection('ads').insertOne(ad, function(err){
           console.log(err);
           console.log("Ad Record added as " + ad._id);
           response.json(ad._id);
@@ -292,12 +161,12 @@ app.post("/ads/", function(request, response){
 });
 
 app.put("/ads/", function(request, response){
-     MongoClient.connect(url, function(err, db) {
+     MongoClient.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
         var ad = request.body;
         ad._id = ObjectId(ad._id);
         console.log(ad);
-        db.collection('ads').update({ _id : ObjectId(ad._id) }, ad, function(err){
+        db.collection('ads').updateOne({ _id : ObjectId(ad._id) }, ad, function(err){
           console.log(err);
           console.log("Ad Record updated as " + ad._id);
           response.json({ "success" : true });
@@ -310,9 +179,9 @@ app.put("/ads/", function(request, response){
 app.delete("/ads/:adId", function(request, response){
      var adIdToDelete = request.params.adId;
     
-     MongoClient.connect(url, function(err, db) {
+     MongoClient.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
-        db.collection('ads').remove({ _id : ObjectId(adIdToDelete) }, function(err){
+        db.collection('ads').removeOne({ _id : ObjectId(adIdToDelete) }, function(err){
           console.log(err);
           console.log("Ad Record delete " + adIdToDelete);
           response.json({ "success" : true });
@@ -325,7 +194,7 @@ app.get("/ads/:adId", function(request, response){
      var adId = request.params.adId;
      console.log(adId);
     
-     MongoClient.connect(url, function(err, db) {
+     MongoClient.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
 
         db.collection('ads').find({ _id : ObjectId(adId) }).toArray(function(err, docs) {
