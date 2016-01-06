76e8eb4c183652c2e34799a48ea56399f1f02142
TomerZv <tomerzv@gmail.com>
2016-01-06 15:50:25 +0200

    add mongo import and change server

--- RunAll.bat
+++ RunAll.bat
@@ -4,7 +4,7 @@ taskkill /F /IM chrome.exe
 start "MongoDB Server" cmd /K C:\"Program Files"\MongoDB\Server\3.2\bin\mongod.exe
 timeout 2
 start "NodeJS ADS Server" /I cmd /K "C:\Program Files\nodejs\node.exe" %CD%\server.js
-timeout 5
+timeout 2
 start "import rows" /I cmd /K start "import rows" /I cmd /K "C:\Program Files\MongoDB\Server\3.2\bin\mongoimport.exe" --db test --collection ads --drop --jsonArray --file %CD%\data.json
 timeout 2
 start chrome.exe --allow-file-access-from-files http://127.0.0.1:80/#/display/1
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var server = http.createServer(app);
 
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
+var mongoDB = require('mongodb').MongoClient;
 var io = require('socket.io')(server);
 var ObjectId = require('mongodb').ObjectID;
 var assert = require('assert');
@@ -25,43 +25,16 @@ server.listen({
 });
 
 var screens = {};
-
-io.sockets.on('connection', function (socket) {
-
-  socket.on('register:screen', function(data){
-    console.log("Screen Registered: " + data.screenId);
-    // we store the screen id in the socket session for this client
-    socket.screenId = data.screenId;
-    // add the screenId to the global list
-    screens[data.screenId] = data.screenId;
-  });
-
-  socket.on('ad:shown', function(data) { 
-     MongoClient.connect(mongoUrl, function(err, db) {
-        assert.equal(null, err);
-        var stat = data;
-        console.log(stat);
-        stat.shownAt = new Date();
-        db.collection('stats').insertOne(stat, function(err){
-          console.log(err);
-          console.log("Statistics Record added as " + stat._id);
-          db.close();
-        });
-    });
-  });
-    
-  socket.on('disconnect', function(){
-    console.log("Screen UnRegistered: " + socket.screenId);
-    // remove the screenId from global screens list
-    delete screens[socket.screenId];
-  });
-
-});
-
 app.use(express.static(__dirname));
 
-app.get("/timePerAd", function(request, response){
-    MongoClient.connect(mongoUrl, function(err, db) {
+
+// Default route
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/timePerAd", function(req, res){
+    mongoDB.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
         
         db.collection('stats').aggregate(
@@ -79,14 +52,14 @@ app.get("/timePerAd", function(request, response){
             stats.forEach(function(item) { 
                 sample.push({'letter' : item._id.title, 'frequency' : item.duration*1.0/60});
             });
-            response.json(sample);
+            res.json(sample);
             db.close();
         });
    });
 });
 
-app.get("/timeUtilizationPerDay", function(request, response){
-    MongoClient.connect(mongoUrl, function(err, db) {
+app.get("/timeUtilizationPerDay", function(req, res){
+    mongoDB.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
         
         db.collection('stats').aggregate(
@@ -104,107 +77,135 @@ app.get("/timeUtilizationPerDay", function(request, response){
             stats.forEach(function(item) { 
                 sample.push({'letter' : item._id.day, 'frequency' : item.duration*1.0/60/60/24});
             });
-            response.json(sample);
+            res.json(sample);
             db.close();
         });
    });
 });
 
-app.get("/ads/", function(request, response){
-     MongoClient.connect(mongoUrl, function(err, db) {
+app.get("/ads/", function(req, res){
+    mongoDB.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
         
         var query = {};
          
-        var screenId = request.query.screenId;
+        var screenId = req.query.screenId;
         if (screenId) {
-            var screens = request.query.screenId.split(',').map(function (x) { 
+            var screens = req.query.screenId.split(',').map(function (x) {
                 return parseInt(x, 10); 
             });
             query = { screenIds : { $in : screens }};
         }
          
-        var days = request.query.days;
+        var days = req.query.days;
         if (days) {
-            var daysParsed = request.query.days.split(',').map(function (x) { 
+            var daysParsed = req.query.days.split(',').map(function (x) {
                 return parseInt(x, 10); 
             });
             query['timeFrames.days'] = { $in : daysParsed };
         }
          
-        var duration = parseInt(request.query.duration);
+        var duration = parseInt(req.query.duration);
         if (duration) {
             query['duration'] = { $gte : duration };
         }
 
         db.collection('ads').find(query).toArray(function(err, docs) {
             assert.equal(null, err);
-            response.json(docs);
+            res.json(docs);
             db.close();
         }); 
     });
 });
 
-app.post("/ads/", function(request, response){
-     MongoClient.connect(mongoUrl, function(err, db) {
+app.post("/ads/", function(req, res){
+    mongoDB.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
-        var ad = request.body;
+        var ad = req.body;
         console.log(ad);
         db.collection('ads').insertOne(ad, function(err){
           console.log(err);
           console.log("Ad Record added as " + ad._id);
-          response.json(ad._id);
+          res.json(ad._id);
           io.sockets.emit('ad:added', ad);
           db.close();
         });
     });
 });
 
-app.put("/ads/", function(request, response){
-     MongoClient.connect(mongoUrl, function(err, db) {
+app.put("/ads/", function(req, res){
+    mongoDB.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
-        var ad = request.body;
+        var ad = req.body;
         ad._id = ObjectId(ad._id);
         console.log(ad);
         db.collection('ads').updateOne({ _id : ObjectId(ad._id) }, ad, function(err){
           console.log(err);
           console.log("Ad Record updated as " + ad._id);
-          response.json({ "success" : true });
+          res.json({ "success" : true });
           io.sockets.emit('ad:updated', ad);
           db.close();
         });
     });
 });
 
-app.delete("/ads/:adId", function(request, response){
-     var adIdToDelete = request.params.adId;
-    
-     MongoClient.connect(mongoUrl, function(err, db) {
+app.delete("/ads/:adId", function(req, res){
+     var adIdToDelete = req.params.adId;
+
+    mongoDB.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
         db.collection('ads').removeOne({ _id : ObjectId(adIdToDelete) }, function(err){
           console.log(err);
           console.log("Ad Record delete " + adIdToDelete);
-          response.json({ "success" : true });
+          res.json({ "success" : true });
           db.close();
         });
     });
 });
 
-app.get("/ads/:adId", function(request, response){
-     var adId = request.params.adId;
+app.get("/ads/:adId", function(req, res){
+     var adId = req.params.adId;
      console.log(adId);
-    
-     MongoClient.connect(mongoUrl, function(err, db) {
+
+    mongoDB.connect(mongoUrl, function(err, db) {
         assert.equal(null, err);
 
         db.collection('ads').find({ _id : ObjectId(adId) }).toArray(function(err, docs) {
             assert.equal(null, err);
-            response.json(docs);
+            res.json(docs);
             db.close();
         });
     });
 });
 
-app.get("/", function(request, response){
-    response.sendFile(__dirname + "/index.html");
+io.sockets.on('connection', function (socket) {
+
+    socket.on('register:screen', function(data){
+        console.log("Screen Registered: " + data.screenId);
+        // we store the screen id in the socket session for this client
+        socket.screenId = data.screenId;
+        // add the screenId to the global list
+        screens[data.screenId] = data.screenId;
+    });
+
+    socket.on('ad:shown', function(data) {
+        mongoDB.connect(mongoUrl, function(err, db) {
+            assert.equal(null, err);
+            var stat = data;
+            console.log(stat);
+            stat.shownAt = new Date();
+            db.collection('stats').insertOne(stat, function(err){
+                console.log(err);
+                console.log("Statistics Record added as " + stat._id);
+                db.close();
+            });
+        });
+    });
+
+    socket.on('disconnect', function(){
+        console.log("Screen UnRegistered: " + socket.screenId);
+        // remove the screenId from global screens list
+        delete screens[socket.screenId];
+    });
+
 });
